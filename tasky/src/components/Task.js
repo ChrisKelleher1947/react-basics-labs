0ba92afeccee34b0754532e41c1f852d033cd9cd
ChrisKelleher1947 <ckelleher1312@gmail.com>
2024-10-04 15:26:37 +0100

    Tasky Lab 2 - Exercises 1 and 2 done

--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -9,6 +9,7 @@ const Task = (props) => {
             <button onClick={props.markDone} className='doneButton'>Done</button>
             <button className='deleteButton' onClick={props.deleteTask}>Delete</button>
             <p className= "description">{props.description}</p>
+            <p className= "priority">{props.priority}</p>
             <p>{props.children}</p>
         </div>
     )
